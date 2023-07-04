c31cff1ae182754f61d34cd38412f5809f20f337
8itwise <supremebigboy@gmail.com>
2023-07-04 02:59:08 +0100

    first commit

--- Server/main.py
+++ Server/main.py
@@ -9,6 +9,7 @@ class MyHandler(BaseHTTPRequestHandler):
 
     # Don't print: 127.0.0.1 - - [22/Jun/2021 21:29:43] "POST / HTTP/1.1" 200
     def log_message(self, format, *args):
+        print("pass")
         pass
 
     def save_file(self, length):
