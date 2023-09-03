a8aaf3361fe7842cd3486252356d75f519f40f68
8itwise <supremebigboy@gmail.com>
2023-09-03 23:55:12 +0100

    minor changes

---
+++ Client/DataHarvester.py
@@ -0,0 +1,43 @@
+import platform
+from getmac import get_mac_address as gma
+import getpass
+from datetime import timedelta
+import datetime
+
+
+
+class DataHarvester:
+
+
+    # returns client system information to the server
+    def get_platform_info(self):
+
+        system_info = {
+                         "mac-address": gma(),
+                         "os": platform.uname().system,
+                         "system-name": platform.uname().node,
+                         "release": platform.uname().release,
+                         "version": platform.uname().version,
+                         "machine": platform.uname().machine,
+                         "date-joined": str(datetime.date.today()),
+                         "time-joined": str(datetime.datetime.now().time()),
+                         "user": getpass.getuser()
+                    }
+
+        return system_info
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
